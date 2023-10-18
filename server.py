dbfeb548bc82d228d8e0ba6514d6e6d2144d7e95
trasmaja <aetlagevik@gmail.com>
2023-10-18 12:23:33 +0200

    working with three gens

--- server.py
+++ server.py
@@ -13,6 +13,9 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 weather_api_key = os.getenv("WEATHER_API_KEY")
+# Initialize chat model
+chat_model = ChatOpenAI(model_name='gpt-3.5-turbo-16k')  # Make sure to set the API key as an environment variable
+
 
 def gen_article_text(topic, env="prod"):
     article_titles = list()
@@ -72,45 +75,45 @@ def get_weather(city):
     return temp, description
 
 def themes_and_sumamry(text):  
-  chat_model = ChatOpenAI(model_name='gpt-3.5-turbo-16k') # Must have set API key as env var
   prompt = f"Identify three key themes in the following text an give each theme an informative title. Return each key theme on a new line. This is the text: {text}"
   themes = chat_model.predict(prompt)
 
   # prompt = f"Based on these three themes {themes} found in this text {text} write a coherent three segment podcast text for a podcaster to read. The listener have already been listening to an introduction so jump right into the segments without introducing them."
   # podcast_transcript = chat_model.predict(prompt)
   
-  prompt = f"Based on these three themes {themes} found in this text {text} write a podcast text for a podcaster to read only for theme 1. The listener have already been listening to an introduction of the themes so no introduction is needed. Only generate the text that will be read for theme 1. Dont introduce the podcast or mention the other themes."
+  prompt = f"Based on these three themes {themes} found in this text {text} write a short podcast text for a podcaster to read only for theme 1. The listener have already been listening to an introduction of the themes so no introduction is needed. Only generate the text that will be read for theme 1. Dont introduce the podcast or mention the other themes."
   theme_1 = chat_model.predict(prompt)
-  prompt = f"Based on these three themes {themes} found in this text {text} write a podcast text for a podcaster to read only for theme 2. The listener have already been listening to an introduction of the themes so no introduction is needed. Only generate the text that will be read for theme 2. Dont introduce the podcast or mention the other themes."
+  prompt = f"Based on these three themes {themes} found in this text {text} write a short podcast text for a podcaster to read only for theme 2. The listener have already been listening to an introduction of the themes so no introduction is needed. Only generate the text that will be read for theme 2. Dont introduce the podcast or mention the other themes."
   theme_2 = chat_model.predict(prompt)
-  prompt = f"Based on these three themes {themes} found in this text {text} write a podcast text for a podcaster to read only for theme 3. The listener have already been listening to an introduction of the themes so no introduction is needed. Only generate the text that will be read for theme 3. Dont introduce the podcast or mention the other themes."
+  prompt = f"Based on these three themes {themes} found in this text {text} write a short podcast text for a podcaster to read only for theme 3. The listener have already been listening to an introduction of the themes so no introduction is needed. Only generate the text that will be read for theme 3. Dont introduce the podcast or mention the other themes."
   theme_3 = chat_model.predict(prompt)
   # print(themes)
   # print(podcast_transcript)
   return themes, theme_1 + theme_2 + theme_3
 
+# def concatenate_audio_moviepy(self, audio_clip_paths, output_path):
+#         """Concatenates several audio files into one audio file using MoviePy
+#         and save it to `output_path`. Note that extension (mp3, etc.) must be added to `output_path`"""
+#         clips = [AudioFileClip(c) for c in audio_clip_paths]
+#         final_clip = concatenate_audioclips(clips)
+#         final_clip.write_audiofile(output_path)
+
 def gen_intro(town, day, time, w_temp, w_desc, topic, themes):
     # Connect to OpenAI, prompt ChatGPT to summarize the text
-    chat_model = ChatOpenAI(model_name='gpt-3.5-turbo-16k') # Must have set API key as env var
     prompt = f"You are going to generate a podcast introduction for the daily news podcast PodPerfect hosted by Zoe. Begin with setting the scene with how the current day is looking. It is {day} and the time is {time} in {town}. The weather is {w_temp} with {w_desc}. Then introduce the {topic}. The three segments in today’s podcast are {themes}. In your response, only include the words Zoe should read and no other text. Make it around 300 words. Begin with “You are listening to PodPerfect – a podcast with me, Zoe, as your AI host.” and end with “Let’s jump right in!”."
     intro = chat_model.predict(prompt)
 
-    prompt = f"You are going to generate a podcast outro for the daily news podcast PodPerfect hosted by Zoe. The three segments in today’s podcast was the following themes {themes}. In your response, only include the words Zoe should read and no other text. Make it around 300 words. End with “You have been listening to PodPerfect – a podcast with me, Zoe your AI host.”"
-    outro = chat_model.predict(prompt)
+    # prompt = f"You are going to generate a podcast outro for the daily news podcast PodPerfect hosted by Zoe. The three segments in today’s podcast was the following themes {themes}. In your response, only include the words Zoe should read and no other text. Make it around 300 words. End with “You have been listening to PodPerfect – a podcast with me, Zoe your AI host.”"
+    # outro = chat_model.predict(prompt)
+    outro = ""
     return intro, outro
 
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-
-@app.route('/create')
-def get_create():
+def gen_news_podcast(topic):
     town = "Stockholm"
     days = ["Monday", "Tuesday", "Wednesday", "Thirsday", "Friday", "Saturday", "Sunday"]
     day = days[datetime.datetime.today().weekday()]
-    time = "9 a.m"
-    topic = request.args.get('topic')
+    time = "6 a.m"
+    
     print("topic")
     print(topic)
     text = gen_article_text(topic, "prod")
@@ -122,6 +125,121 @@ def get_create():
     intro, outro = gen_intro(town, day, time, w_temp, w_desc, topic, themes)
     full_text = intro + "\n" + podcast_transcript + "\n" + outro
     print(full_text)
-    return jsonify(full_text)
+    return full_text
+
+def gen_doc_podcast(user_prompt, num_words):
+    # if not os.path.exists("documentary_audio"):
+    #     os.makedirs("documentary_audio")
+    text = ""
+    prompt = "Create the outline/structure for a {} word long documentary about {}. Specify how many words each section should have. Return your answer in this format '*****Topic 1, *****Topic 2, *****Topic 3'".format(num_words, user_prompt) 
+    response = chat_model.predict(prompt)
+    split_response = response.split('*****')
+    sections = []
+    for part in split_response:
+        if len(part) > 10:
+            sections.append(part)
+    chapter_length = int(num_words / len(sections))
+    file_names = []
+    for i in range(len(sections)):
+        print(i)
+        if i == 0:
+            prompt = "Write the introduction for this documentary about {}, focus on this part {}. Use approximatly {} number of words in your output. ".format(user_prompt, sections[0], chapter_length) 
+        else:
+            prompt = "You are writing a text about {} and now you are going to write about this sub-topic {}. Use approximatly {} number of words in your output. Start with 'Part {}: ' + the headline for this sub-topic".format(user_prompt, sections[i], chapter_length, i) 
+        response = chat_model.predict(prompt)
+        # audio = generate(text=response, voice="Adam", model="eleven_monolingual_v1", api_key=ELEVEN_API_KEY)
+        # save(audio, "documentary_audio/documentary_in_{}_mins_{}.wav".format(num_minutes, i))
+        text += "\n" + response + "\n"
+        # file_names.append("documentary_audio/documentary_in_{}_mins_{}.wav".format(num_minutes, i))
+        # file_names.append("sound_effects/original_transition.wav")
+    # concatenate_audio_moviepy(file_names, "documentary_audio/documentary_about_{}_in_{}_mins.wav".format(user_prompt, num_minutes))
+    
+    return text
+
+def gen_story_podcast(user_prompt, num_words):
+    # Save audiofile
+    # if not os.path.exists("story_audio"):
+    #     os.makedirs("story_audio")
+    prompt = "How many chapters are appropriate for a {} word long story? Return just the specified number".format(num_words)
+    response = chat_model.predict(prompt)
+    # Hårdkoda som lista istället <---- daniel
+    try:
+        chapters = int(response)
+    except:
+        if num_words < 1000:
+            chapters = 1
+        else:
+            chapters = int(num_words / 500)
+    print(chapters)
+    text = ""
+    prompt = "Create the outline/chapters for a {} word long story with {} chapters about {}. Specify how many words each section should have. Return your answer in the following format '*****Chapter 1, *****Chapter 2, *****Chapter 3'".format(num_words, chapters, user_prompt) 
+    print(prompt)
+    response = chat_model.predict(prompt)
+    print("----")
+    print(response)
+
+    prompt = "Summarise what the story is about: {}".format(response)
+    topic = chat_model.predict(prompt)
+
+    split_response = response.split('*****')
+    sections = []
+    for part in split_response:
+        if len(part) > 10:
+            sections.append(part)
+    
+    # chapter_length = int(num_words / len(sections))
+    chapter_length = int(num_words / chapters)
+    print(sections)
+    # file_names = []
+    summary = ""
+    for i in range(len(sections)):
+        print(i)
+        print(111111)
+        prompt = "You are writing a story about {} and now you are going to write about this chapter {}. This has happened so far in the story: '{}'. Use approximatly {} number of words in your output. Start with 'Chapter X: ' + the headline for this chapter".format(topic, sections[i], summary, chapter_length) 
+        print(prompt)
+        print(22222)
+        response = chat_model.predict(prompt)
+        print(response)
+        print('*****')
+        prompt = "Summarise: {}".format(response)
+        summary = chat_model.predict(prompt)
+        #audio = generate(text=response, voice="Adam", model="eleven_monolingual_v1", api_key=ELEVEN_API_KEY)
+        #save(audio, "story_audio/story_about_{}_in_{}_mins_{}.wav".format(user_prompt, num_minutes, i))
+        text += "\n" + response + "\n"
+        
+        #file_names.append("story_audio/story_about_{}_in_{}_mins_{}.wav".format(user_prompt, num_minutes, i))
+        #file_names.append("sound_effects/original_transition.wav")
+    return text
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/create')
+def get_create():
+    # Argument fetched from url params
+    user_prompt = request.args.get('topic')
+    num_minutes = request.args.get('min')
+    num_words = int(num_minutes)*135
+    prompt = "Can you create a documentary/news broadcast/podcast/fantasy story about this input? Respond only by returning 'YES' or explain why it does not work. Input = {}".format(user_prompt)
+    response = chat_model.predict(prompt)
+    if "YES" != response:
+        return print(response)
+
+    prompt = "What category would you classify this input trying to create? Respond just by returning 'NEWS', 'STORY' or 'DOCUMENTARY' Input = {}".format(user_prompt)
+    category = chat_model.predict(prompt)
+    
+    if category == "NEWS":
+        podcast_text = gen_news_podcast(user_prompt)
+        return jsonify(podcast_text)
+    elif category == "DOCUMENTARY":
+        podcast_text = gen_doc_podcast(user_prompt, num_words)
+        return jsonify(podcast_text)
+    else:
+        podcast_text = gen_story_podcast(user_prompt, num_words)
+        return jsonify(podcast_text)
+    
+
 
 app.run(host='0.0.0.0', port=3000)
